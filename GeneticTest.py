87bec6ee78b6c8ac4110c096fda1a752fe1f785a
Idriswafdi <wafdi-37@hotmail.fr>
2020-12-24 14:35:03 +0100

    update 2

--- GeneticTest.py
+++ GeneticTest.py
@@ -3,6 +3,7 @@ import random
 from deap import base
 from deap import creator
 from deap import tools
+from statistics import mean
 from math import sin
 
 creator.create("FitnessMax", base.Fitness, weights=(1.0, 1.0))
@@ -20,11 +21,12 @@ toolbox.register("audience", random.uniform, 1.00, 5.00)
 toolbox.register("alloue", random.randint, 0, 1)
 
 # Nombres d'ecrans dans un individu
-N_Screen = 10
+N_Screen = 4
 
 # Structure initializers
 toolbox.register("individual", tools.initCycle, creator.Individual,
-                 (toolbox.prix, toolbox.jour, toolbox.weekend, toolbox.mois, toolbox.horaire, toolbox.audience, toolbox.alloue), n=N_Screen)
+                 (toolbox.prix, toolbox.jour, toolbox.weekend, toolbox.mois,
+                  toolbox.horaire, toolbox.audience, toolbox.alloue), n=N_Screen)
 
 # define the population to be a list of individuals
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -50,8 +52,6 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
     # create an initial population of 300 individuals (where
     # each individual is a list of integers)
     pop = toolbox.population(n=population_size)
-    for i in pop:
-        print(i[6::7])
 
 
     # CXPB  is the probability with which two individuals
@@ -61,26 +61,26 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
     CXPB = CrossoverP
     MUTPB = MutationP
 
+# ----------------------------------------------------PREMIERE EVAL---------------------------------------------
     def feasible(individual):
         """Feasibility function for the individual. Returns True if feasible False
     otherwise."""
-
-        if individual[0] < budget_size:
+        if sum(individual[0::7]) < budget_size:
             return True
         return False
 
     def evalFct(individual, adaptiveP):
         """Evaluation function for the individual."""
-        prixInd = individual[0]
-        audienceInd = individual[5]
+        prixInd = individual[0::7]
+        audienceInd = individual[5::7]
 
         if feasible(individual):
-            return prixInd, audienceInd
+            return sum(prixInd), mean(audienceInd)
         else:
 
-            return prixInd - adaptiveP, audienceInd - adaptiveP/1000
+            return sum(prixInd) - adaptiveP, mean(audienceInd) - adaptiveP/1000
 
-    toolbox.register("evaluate", evalFct, adaptiveP = PenAdapt)
+    toolbox.register("evaluate", evalFct, adaptiveP=PenAdapt)
 
     print("Start of evolution")
 
@@ -103,7 +103,7 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
     comptAug = 0
     comptDiv = 0
 
-    # Begin the evolution
+# ----------------------------------------------------DEBUT EVOLUTION---------------------------------------------
     while g < 100:
         # A new generation
         g = g + 1
@@ -121,36 +121,39 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
         # Clone the selected individuals
         offspring = list(map(toolbox.clone, offspring))
 
-        # Apply crossover and mutation on the offspring
+# ----------------------------------------------------CROSSOVER----------------------------------------------------
         for child1, child2 in zip(offspring[::2], offspring[1::2]):
+
 
             # cross two individuals with probability CXPB
             if random.random() < CXPB:
+                print("before crossover %s, %s: " % (child1, child2))
                 toolbox.mate(child1, child2)
+                print("new crossover %s, %s: " % (child1, child2))
 
                 # fitness values of the children
                 # must be recalculated later
                 del child1.fitness.values
                 del child2.fitness.values
-
+# ----------------------------------------------------MUTATION----------------------------------------------------
         for mutant in offspring:
             alloc = mutant[6::7]
             # mutate an individual with probability MUTPB
-            for y in alloc:
+            for y in range(len(alloc)):
                 if random.random() < MUTPB:
-                    print("mutation a eu lieu ! avant: %s" % mutant[6::7])
+                    #print("mutation a eu lieu ! avant: %s" % mutant[6::7])
                     if alloc[y] == 1:
-                        print("avant c'etait un %s" % alloc[y])
+                        #print("avant c'etait un %s" % alloc[y])
                         alloc[y] = 0
-                        print("maintenant c'est un %s" % alloc[y])
+                        #print("maintenant c'est un %s" % alloc[y])
                     else:
-                        print("avant c'etait un %s" % alloc[y])
+                        #print("avant c'etait un %s" % alloc[y])
                         alloc[y] = 1
-                        print("maintenant c'est un %s" % alloc[y])
+                        #print("maintenant c'est un %s" % alloc[y])
                     mutant[6::7] = alloc
-                    print("mutation a eu lieu ! apres: %s" % mutant[6::7])
+                    #print("mutation a eu lieu ! apres: %s" % mutant[6::7])
                     del mutant.fitness.values
-
+# ----------------------------------------------------RECALCUL FITNESS---------------------------------------------
         # Evaluate the individuals with an invalid fitness
         invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
         fitnesses = map(toolbox.evaluate, invalid_ind)
@@ -161,13 +164,15 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
 
         # The population is entirely replaced by the offspring
         pop[:] = offspring
+        best_ind = tools.selBest(pop, 1)[0]
+        print("Best individual has %s, %s" % (sum(best_ind[0::7]), mean(best_ind[5::7])))
 
         # Gather all the fitnesses in one list and print the stats
         fits = [ind.fitness.values[0] for ind in pop]
 
         BestOne = tools.selBest(pop, 5)
         #print("  Best 3 solutions %s" % BestOne)
-
+# --------------------------------------------------------FEEDBACK---------------------------------------------------
         # si les 3 best sont faisables
         if all(feasible(i) for i in BestOne):
             PenAdapt = PenAdapt/2
@@ -185,18 +190,18 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
             print(" Aucun changement de pénalité ! %s" % PenAdapt)
             comptDiv = comptDiv + 1
 
-
+# ----------------------------------------------------MODIF PENALITE---------------------------------------------------
 
         # fitness function avec pénalité adaptative
         def adaptiveEval(individual, adaptiveP):
-            prixInd = individual[0]
-            audienceInd = individual[5]
+            prixInd = individual[0::7]
+            audienceInd = individual[5::7]
 
             if feasible(individual):
-                return prixInd, audienceInd
+                return sum(prixInd), mean(audienceInd)
             else:
 
-                return prixInd - adaptiveP, audienceInd - adaptiveP / 1000
+                return sum(prixInd) - adaptiveP, mean(audienceInd) - adaptiveP / 1000
 
         toolbox.register("evaluate", adaptiveEval, adaptiveP=PenAdapt)
 
@@ -212,10 +217,12 @@ def GenAlg(CrossoverP, MutationP, PenAdapt, population_size, budget_size):
     print(" Pénalité diminuée %s fois" % comptDim)
     print(" Pénalité non changée %s fois" % comptDiv)
     print(" Pénalité %s" % PenAdapt)
-    #print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+    print("Best individual has %s, %s" % (sum(best_ind[0::7]), mean(best_ind[5::7])))
     #print("Worst individual is %s, %s" % (worst_ind, worst_ind.fitness.values))
 
 
 
 if __name__ == "__main__":
-    GenAlg(0.5, 0.1, 100, 10, 100)
+    # CrossoverP, MutationP, PenAdapt, population_size, budget_size
+    GenAlg(0.5, 0.1, 100, 10, 2000)
